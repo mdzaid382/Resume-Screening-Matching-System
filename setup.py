fa1c75379979be7cb4954feadaec3fedfce1dcfe
mdzaid382 <mdzaid382@gmail.com>
2026-06-02 17:15:59 +0530

    essential folder created using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is end to end resume screening and matching project.',
+    author='mdzaid382',
+    license='',
+)
