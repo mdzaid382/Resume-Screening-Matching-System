fc40c9ecdf4cb30c3e9897edb0888a846b6408c7
mdzaid382 <mdzaid382@gmail.com>
2026-06-04 15:33:13 +0530

    data ingestion and data preprocessing added to pipeline

---
+++ src/data/data_ingestion.py
@@ -0,0 +1,76 @@
+# data ingestion
+import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
+import os
+from sklearn.model_selection import train_test_split
+import yaml
+from src.logger import logging
+# from src.connections import s3_connection
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logging.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logging.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logging.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error: %s', e)
+        raise
+
+def load_data(data_path: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
+        df = pd.read_csv(data_path)
+        logging.info('Data loaded from %s', data_path)
+        return df
+    except pd.errors.ParserError as e:
+        logging.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error occurred while loading the data: %s', e)
+        raise
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+    """Save the train and test datasets."""
+    try:
+        raw_data_path = os.path.join(data_path, 'raw')
+        os.makedirs(raw_data_path, exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path, "train.csv"), index=False)
+        test_data.to_csv(os.path.join(raw_data_path, "test.csv"), index=False)
+        logging.info('Train and test data saved to %s', raw_data_path)
+    except Exception as e:
+        logging.error('Unexpected error occurred while saving the data: %s', e)
+        raise
+
+def main():
+    try:
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        
+        
+        df = load_data(data_path=r'D:\Resume-Screening-Matching-System\data_upload_S3\data\cleaned_dataset.csv')
+        # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
+        # df = s3.fetch_file_from_s3("data.csv")
+
+
+
+        train_data, test_data = train_test_split(df, test_size=test_size, random_state=42)
+        save_data(train_data, test_data, data_path='./data')
+    except Exception as e:
+        logging.error('Failed to complete the data ingestion process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -1,30 +1,123 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+# data preprocessing
+
+import pandas as pd
+import os
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from src.logger import logging
+from sklearn.preprocessing import LabelEncoder
+nltk.download('wordnet')
+nltk.download('stopwords')
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+
+
+
+def clean_text(text: str) -> str:
+
+    # Initialize lemmatizer and stopwords
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words("english"))
+
+    # Remove email addresses
+    text = re.sub(r'\S+@\S+', ' ', text)
+
+    # Remove URLs (linkedin, github, websites)
+    text = re.sub(r'http\S+|www\.\S+|linkedin\.com/\S+|github\.com/\S+', ' ', text)
+
+    # Remove phone numbers
+    text = re.sub(
+        r'(\+\d{1,3}[-.\s]?)?(\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4})',
+        ' ',
+        text
+    )
+
+    # Remove markdown links
+    text = re.sub(r'\[([^\]]+)\]\([^)]+\)', r'\1', text)
+
+    # Remove bullet symbols
+    text = re.sub(r'[•▪◦●■♦★]', ' ', text)
+
+    # Remove asterisks used as bullets
+    text = re.sub(r'\*', ' ', text)
+
+    # Keep only letters, numbers, +, #, /, . and spaces
+    text = re.sub(r'[^a-zA-Z0-9+#/. ]', ' ', text)
+
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove extra spaces
+    text = re.sub(r'\s+', ' ', text).strip()
+    
+    #remove stopwords
+    text = " ".join([word for word in text.split() if word not in stop_words])
+
+    #lematize the text
+    text = " ".join([lemmatizer.lemmatize(word) for word in text.split()])
+   
+
+    return text
+
+
+def preprocess_dataframe(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    Preprocess a DataFrame by applying text preprocessing to a specific column.
 
+    Args:
+        df (pd.DataFrame): The DataFrame to preprocess.
+        col (str): The name of the column containing text.
+
+    Returns:
+        pd.DataFrame: The preprocessed DataFrame.
+    """
+
+    # Apply preprocessing to the specified column
+    df[col_name] = df[col_name].apply(clean_text)
+
+    # Drop rows with NaN values
+    df = df.dropna(subset=[col_name])
+    logging.info("Data pre-processing completed")
+    return df
+
+
+def labelencoder(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
+
+    le = LabelEncoder()
+    df[col_name] = le.fit_transform(df[col_name])
+    return df
+
+
+def main():
+    try:
+        # Fetch the data from data/raw
+        train_data = pd.read_csv('./data/raw/train.csv')
+        test_data = pd.read_csv('./data/raw/test.csv')
+        logging.info('data loaded properly')
+
+        # Transform the data
+        train_processed_data = preprocess_dataframe(train_data, 'Resume')
+        test_processed_data = preprocess_dataframe(test_data, 'Resume')
+        logging.info('data processed completed.')
+
+        #labels the target column
+        train_labeled_data = labelencoder(train_processed_data, 'Role')
+        test_labeled_data = labelencoder(test_processed_data, 'Role')
+        logging.info('target column labeled.')
+
+        # Store the data inside data/processed
+        data_path = os.path.join("./data", "interim")
+        os.makedirs(data_path, exist_ok=True)
+        
+        train_labeled_data.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
+        test_labeled_data.to_csv(os.path.join(data_path, "test_processed.csv"), index=False)
+        
+        logging.info('Processed data saved to %s', data_path)
+    except Exception as e:
+        logging.error('Failed to complete the data transformation process: %s', e)
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
