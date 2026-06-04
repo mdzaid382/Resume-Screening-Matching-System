d72ddc11f34f3f10b6a0a0772e4444010c0d2146
mdzaid382 <mdzaid382@gmail.com>
2026-06-04 16:40:59 +0530

    model building code added to pipeline

---
+++ src/models/model_building.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.svm import LinearSVC
+import yaml
+from src.logger import logging
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logging.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logging.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logging.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error: %s', e)
+        raise
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
+        df = pd.read_csv(file_path)
+        logging.info('Data loaded from %s', file_path)
+        return df
+    except pd.errors.ParserError as e:
+        logging.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error occurred while loading the data: %s', e)
+        raise
+
+def train_model(X_train: np.ndarray, y_train: np.ndarray) -> LinearSVC:
+    """Train the Logistic Regression model."""
+    try:
+        params = load_params('params.yaml')
+        C = params['model_building']['C']
+        penalty = params['model_building']['penalty']
+        clf = LinearSVC(C=C, penalty=penalty)
+        clf.fit(X_train, y_train)
+        logging.info('Model training completed')
+        return clf
+    except Exception as e:
+        logging.error('Error during model training: %s', e)
+        raise
+
+def save_model(model, file_path: str) -> None:
+    """Save the trained model to a file."""
+    try:
+        with open(file_path, 'wb') as file:
+            pickle.dump(model, file)
+        logging.info('Model saved to %s', file_path)
+    except Exception as e:
+        logging.error('Error occurred while saving the model: %s', e)
+        raise
+
+def main():
+    try:
+
+        train_data = load_data('./data/processed/train_tfidf.csv')
+        X_train = train_data.iloc[:, :-1].values
+        y_train = train_data.iloc[:, -1].values
+
+        clf = train_model(X_train, y_train)
+        
+        save_model(clf, 'models/model.pkl')
+    except Exception as e:
+        logging.error('Failed to complete the model building process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
